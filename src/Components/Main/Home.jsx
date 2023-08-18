f5a19c878b552d9392e01d1d7024350ea3e080c5
Michael Okorie <mickienedu@gmail.com>
2023-08-18 23:16:01 +0100

    Mike Update

--- src/Components/Main/Home.jsx
+++ src/Components/Main/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Hero from '../Hero/Hero'
-import Forgot_pass from '../Forgot_pass/Forgot_pass'
+// import Forgot_pass from '../Forgot_pass/Forgot_pass'
 
 const Home = () => {
   return (
